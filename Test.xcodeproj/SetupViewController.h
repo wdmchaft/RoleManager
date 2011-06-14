90e217509958f6672be4011fe8623cb467189e0d
Rob Little <rob@roblittle.ca>
2011-06-13 20:06:23 -0700

    first xcode based commit

---
+++ Test.xcodeproj/SetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetupViewController.h
+//  Test
+//
+//  Created by Robert Little on 11-06-13.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetupViewController : UIViewController
+
+@end
